df6f388a2c9d0f03284f3a29b3e916fb7d7483cf
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-15 13:42:07 +0200

    Define hero object

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,23 @@
 // Write your JS here
+let hero = {
+    name: "",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "",
+        damage: 2
+    }
+}
+
+hero.assign()
+function rest(newHero) {
+    console.log(hero.health = 10)
+    return hero.health = 10
+}
+
+function pickUpItem(newHero, ) {
+}
+
+function equipWeapon(newHero) {
+}
